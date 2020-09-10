1809e517af7b37ebfbacade34ce01c30b6ac118f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-10 13:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ document.addEventListener("DOMContentLoaded", function(){
 console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered ")
 );
 
-const DOMContentLoaded = document.getElementByID("p#text")
+const DOMContentLoaded = document.getElementByID("p#text");
